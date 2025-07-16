908ddc01b8cdb38c7851a2a66f45d926ed095b81
HekmatSuha <76567609+HekmatSuha@users.noreply.github.com>
2025-07-16 13:31:17 +0600

    Add language selector and i18n support

--- teacher_dashboard/settings.py
+++ teacher_dashboard/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
                 'students.context_processors.student_profile_picture',
             ],
         },
--- teacher_dashboard/urls.py
+++ teacher_dashboard/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('students.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
