0aaa32cc25cca2f4ecf9f343de521b2ef205d592
HekmatSuha <76567609+HekmatSuha@users.noreply.github.com>
2025-07-16 18:14:51 +0600

    Deduplicate and reorder middleware

--- teacher_dashboard/settings.py
+++ teacher_dashboard/settings.py
@@ -43,14 +43,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.locale.LocaleMiddleware", 
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
 ]
 
 ROOT_URLCONF = "teacher_dashboard.urls"
